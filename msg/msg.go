ccb2288a301a6440f7622b2ec16390a59f9a19bb
ortutay <marcell.ortutay@gmail.com>
2014-02-12 23:08:18 -0800

    remove deprecated code

--- msg/msg.go
+++ msg/msg.go
@@ -1,15 +1,12 @@
 package msg
 
 import (
+	"bufio"
 	"bytes"
-	"encoding/base64"
-	"encoding/gob"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"bufio"
 )
 
 var _ = fmt.Printf
@@ -23,12 +20,11 @@ const (
 )
 
 type PaymentAddr struct {
-	Currency Currency
-	Addr     string
+	Currency Currency `json:"currency"`
+	Addr     string   `json:"addr"`
 }
 
-func (pa *PaymentAddr) ToString() string {
-	// TODO(ortutay): figure out real wire format
+func (pa *PaymentAddr) String() string {
 	b, err := json.Marshal(pa)
 	if err != nil {
 		panic(err)
@@ -37,7 +33,6 @@ func (pa *PaymentAddr) ToString() string {
 }
 
 func NewPaymentAddr(str string) (*PaymentAddr, error) {
-	// TODO(ortutay): figure out real wire format
 	var pa PaymentAddr
 	err := json.Unmarshal([]byte(str), &pa)
 	if err != nil {
@@ -55,12 +50,11 @@ const (
 )
 
 type PaymentValue struct {
-	Amount   int64 `json:"amount"`
+	Amount   int64    `json:"amount"`
 	Currency Currency `json:"currency"`
 }
 
-func (pv *PaymentValue) ToString() string {
-	// TODO(ortutay): figure out real wire format
+func (pv *PaymentValue) String() string {
 	b, err := json.Marshal(pv)
 	if err != nil {
 		panic(err)
@@ -69,7 +63,6 @@ func (pv *PaymentValue) ToString() string {
 }
 
 func NewPaymentValue(str string) (*PaymentValue, error) {
-	// TODO(ortutay): figure out real wire format
 	var pv PaymentValue
 	err := json.Unmarshal([]byte(str), &pv)
 	if err != nil {
@@ -81,16 +74,16 @@ func NewPaymentValue(str string) (*PaymentValue, error) {
 
 // TODO(ortutay): add types as appropriate
 type OcReq struct {
-	Id       []string `json:"id,omitempty"`
-	Sig          []string `json:"sig,omitempty"`
-	Nonce        string `json:"nonce,omitempty"`
-	Service      string `json:"service"`
-	Method       string `json:"method"`
-	Args         []string `json:"args,omitempty"`
-	PaymentType  PaymentType `json:"paymentType,omitempty"`
+	Id           []string      `json:"id,omitempty"`
+	Sig          []string      `json:"sig,omitempty"`
+	Nonce        string        `json:"nonce,omitempty"`
+	Service      string        `json:"service"`
+	Method       string        `json:"method"`
+	Args         []string      `json:"args,omitempty"`
+	PaymentType  PaymentType   `json:"paymentType,omitempty"`
 	PaymentValue *PaymentValue `json:"paymentValue,omitempty"`
-	PaymentTxn   string `json:"paymentTxn,omitempty"`
-	Body         []byte `json:"-"`
+	PaymentTxn   string        `json:"paymentTxn,omitempty"`
+	Body         []byte        `json:"-"`
 }
 
 func (r *OcReq) WriteSignablePortion(w io.Writer) error {
@@ -108,65 +101,25 @@ func (r *OcReq) WriteSignablePortion(w io.Writer) error {
 }
 
 func (r *OcReq) Write(w io.Writer) error {
-	b, err := json.Marshal(r)
-	if err != nil {
-		return fmt.Errorf("Error while marshaling to json: %v", err.Error())
-	}
-	_, err = w.Write(b)
-	if err != nil {
-		return fmt.Errorf("Error while writing: %v", err.Error())
-	}
-	_, err = w.Write([]byte("\n"))
-	if err != nil {
-		return fmt.Errorf("Error while writing: %v", err.Error())
-	}
-	if len(r.Body) > 0 {
-		_, err = w.Write(r.Body)
-		if err != nil {
-			return fmt.Errorf("Error while writing: %v", err.Error())
-		}
-	}
-	return nil
+	return writeMsg(r, r.Body, w)
 }
 
 func ReadOcReq(r *bufio.Reader) (*OcReq, error) {
-	jsonLine, err  := r.ReadBytes('\n')
+	jsonLine, body, err := readMsg(r)
 	if err != nil {
-		return nil, fmt.Errorf("error while reading req JSON line: %v", err.Error())
-	}
-	body, err := ioutil.ReadAll(r)
-	if err != nil {
-		return nil, fmt.Errorf("error while reading req body: %v", err.Error())
+		return nil, err
 	}
 	var req OcReq
+	req.Body = body
 	err = json.Unmarshal(jsonLine, &req)
 	if err != nil {
-		return nil, fmt.Errorf("error while unmarshalling req: %v", err.Error())
+		return nil, fmt.Errorf("error while unmarshalling: %v", err.Error())
 	}
-	req.Body = body
 	return &req, nil
 }
 
 func (r *OcReq) String() string {
-	b, err := json.Marshal(r)
-	if err != nil {
-		return ""
-	}
-	var buf bytes.Buffer
-	err = json.Indent(&buf, b, "", "  ")
-	if err != nil {
-		return ""
-	}
-	if len(r.Body) > 0 {
-		return buf.String() + "\n" + string(r.Body)
-	} else {
-		return buf.String() + "\n"
-	}
-}
-
-// TODO(ortutay): WriteEncoded(w io.Writer)
-func (r *OcReq) Encode() ([]byte, error) {
-	return encode(r)
+	return msgString(r, r.Body)
 }
 
 func (r *OcReq) IsSigned() bool {
@@ -194,7 +147,7 @@ const (
 	INVALID_SIGNATURE   = CLIENT_ERROR + "/invalid-signature"
 	SERVICE_UNSUPPORTED = CLIENT_ERROR + "/service-unsupported"
 	METHOD_UNSUPPORTED  = CLIENT_ERROR + "/method-unsupported"
-	INVALID_ARGUMENTS  = CLIENT_ERROR + "/invalid-arguments"
+	INVALID_ARGUMENTS   = CLIENT_ERROR + "/invalid-arguments"
 
 	SERVER_ERROR = "server-error"
 
@@ -208,17 +161,17 @@ const (
 )
 
 type OcResp struct {
-	NodeId []string
-	Sig    []string
-	Nonce  string
-	Status OcRespStatus
+	Id     []string     `json:"id,omitempty"`
+	Sig    []string     `json:"sig,omitempty"`
+	Nonce  string       `json:"nonce,omitempty"`
+	Status OcRespStatus `json:"status,omitempty"`
 	// TODO(ortutay): status code
-	Body []byte
+	Body []byte `json:"-"`
 }
 
 func NewRespOk(body []byte) *OcResp {
 	resp := OcResp{
-		NodeId: []string{},
+		Id:     []string{},
 		Sig:    []string{},
 		Nonce:  "", // TODO(ortutay)
 		Status: OK,
@@ -231,9 +184,8 @@ func NewRespError(status OcRespStatus) *OcResp {
 	if status == OK {
 		panic("got status OK, but expected an error status")
 	}
-
 	resp := OcResp{
-		NodeId: []string{},
+		Id:     []string{},
 		Sig:    []string{},
 		Nonce:  "", // TODO(ortutay)
 		Status: status,
@@ -241,44 +193,75 @@ func NewRespError(status OcRespStatus) *OcResp {
 	return &resp
 }
 
-func (r *OcResp) Encode() ([]byte, error) {
-	return encode(r)
+func (r *OcResp) Write(w io.Writer) error {
+	return writeMsg(r, r.Body, w)
 }
 
-func encode(m interface{}) ([]byte, error) {
-	// TODO(ortutay): for now, just doing gob->base64 to encode; will need
-	// to figure out what to actually do
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-	err := enc.Encode(m)
+func ReadOcResp(r *bufio.Reader) (*OcResp, error) {
+	jsonLine, body, err := readMsg(r)
 	if err != nil {
-		return []byte{}, errors.New("couldn't encode")
+		return nil, err
 	}
-	b64 := base64.StdEncoding.EncodeToString(buf.Bytes())
-	return []byte(b64), nil
-}
-
-func DecodeReq(b64 string) (*OcReq, error) {
-	var req OcReq
-	err := decode(b64, &req)
-	return &req, err
-}
-
-func DecodeResp(b64 string) (*OcResp, error) {
 	var resp OcResp
-	err := decode(b64, &resp)
-	return &resp, err
+	resp.Body = body
+	err = json.Unmarshal(jsonLine, &resp)
+	if err != nil {
+		return nil, fmt.Errorf("error while unmarshalling: %v", err.Error())
+	}
+	return &resp, nil
 }
 
-func decode(b64 string, d interface{}) error {
-	buf, err := base64.StdEncoding.DecodeString(b64)
+func (r *OcResp) String() string {
+	return msgString(r, r.Body)
+}
+
+func writeMsg(v interface{}, body []byte, w io.Writer) error {
+	b, err := json.Marshal(v)
 	if err != nil {
-		return err
+		return fmt.Errorf("Error while marshaling to json: %v", err.Error())
 	}
-	dec := gob.NewDecoder(bytes.NewBuffer(buf))
-	err = dec.Decode(d)
+	_, err = w.Write(b)
 	if err != nil {
-		return err
+		return fmt.Errorf("Error while writing: %v", err.Error())
+	}
+	_, err = w.Write([]byte("\n"))
+	if err != nil {
+		return fmt.Errorf("Error while writing: %v", err.Error())
+	}
+	if len(body) > 0 {
+		_, err = w.Write(body)
+		if err != nil {
+			return fmt.Errorf("Error while writing: %v", err.Error())
+		}
 	}
 	return nil
+}
+
+func readMsg(r *bufio.Reader) ([]byte, []byte, error) {
+	jsonLine, err := r.ReadBytes('\n')
+	if err != nil {
+		return nil, nil, fmt.Errorf("error while reading JSON line: %v", err.Error())
+	}
+	body, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error while reading body: %v", err.Error())
+	}
+	return jsonLine, body, nil
+}
+
+func msgString(v interface{}, body []byte) string {
+	b, err := json.Marshal(v)
+	if err != nil {
+		return ""
+	}
+	var buf bytes.Buffer
+	err = json.Indent(&buf, b, "", "  ")
+	if err != nil {
+		return ""
+	}
+	if len(body) > 0 {
+		return buf.String() + "\n" + string(body)
+	} else {
+		return buf.String() + "\n"
+	}
 }
--- msg/msg_test.go
+++ msg/msg_test.go
@@ -1,23 +1,23 @@
 package msg
 
-import(
+import (
 	"bufio"
-	"testing"
 	"bytes"
+	"testing"
 )
 
-func TestEncodeDecodeOcReq(t *testing.T) {
+func TestReadWriteOcReq(t *testing.T) {
 	req := OcReq{
-		Id: []string{"id1", "id2", "id3"},
-		Sig: []string{"sig1", "sig2", "sig3"},
-		Nonce: "abcnonce",
-		Service: "testService",
-		Method: "testMethod",
-		Args: []string{"1", "2"},
-		PaymentType: ATTACHED,
+		Id:           []string{"id1", "id2", "id3"},
+		Sig:          []string{"sig1", "sig2", "sig3"},
+		Nonce:        "abcnonce",
+		Service:      "testService",
+		Method:       "testMethod",
+		Args:         []string{"1", "2"},
+		PaymentType:  ATTACHED,
 		PaymentValue: &PaymentValue{Amount: 1e8, Currency: BTC},
-		PaymentTxn: "base64-btc-txn",
-		Body: []byte("some body, just a string here, but could be binary data"),
+		PaymentTxn:   "base64-btc-txn",
+		Body:         []byte("some body, just a string here, but could be binary data"),
 	}
 	buf := bytes.Buffer{}
 	err := req.Write(&buf)
@@ -31,5 +31,28 @@ func TestEncodeDecodeOcReq(t *testing.T) {
 	}
 	if req.String() != req2.String() {
 		t.Fatalf("%v != %v\n", req.String(), req2.String())
+	}
+}
+
+func TestReadWriteOcResp(t *testing.T) {
+	resp := OcResp{
+		Id:     []string{"id1", "id2", "id3"},
+		Sig:    []string{"sig1", "sig2", "sig3"},
+		Nonce:  "abcnonce",
+		Status: OK,
+		Body:   []byte("some body, just a string here, but could be binary data"),
+	}
+	buf := bytes.Buffer{}
+	err := resp.Write(&buf)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	b := bufio.NewReader(&buf)
+	resp2, err := ReadOcResp(b)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if resp.String() != resp2.String() {
+		t.Fatalf("%v != %v\n", resp.String(), resp2.String())
 	}
 }
