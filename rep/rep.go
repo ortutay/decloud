28e12dcda7b17b41f7e7104ec9a5ad37c378b0f5
ortutay <marcell.ortutay@gmail.com>
2014-03-05 20:48:34 -0800

    implement SuccessRate

--- rep/rep.go
+++ rep/rep.go
@@ -15,7 +15,6 @@ import (
 )
 
 type Status string
-
 func (s Status) String() string {
 	return string(s)
 }
@@ -34,7 +33,7 @@ type Record struct {
 	OcID         msg.OcID
 	Status       Status
 	PaymentType  msg.PaymentType
-	PaymentValue msg.PaymentValue
+	PaymentValue *msg.PaymentValue
 	Perf         interface{} // Service specific
 }
 
@@ -43,7 +42,7 @@ type Cursor interface {
 	Reset()
 }
 
-func Put(rec *Record) (int64, error) {
+func openOrCreate() (*sql.DB, error) {
 	exists := true
 	if _, err := os.Stat(sqliteDBPath()); os.IsNotExist(err) {
 		exists = false
@@ -51,19 +50,27 @@ func Put(rec *Record) (int64, error) {
 
 	db, err := sql.Open("sqlite3", sqliteDBPath())
 	if err != nil {
-		return 0, fmt.Errorf("error while opening db %v: %v",
+		return nil, fmt.Errorf("error while opening db %v: %v",
 			sqliteDBPath(), err.Error())
 	}
-	defer db.Close()
 
 	if !exists {
 		err = initTable(db)
 		if err != nil {
-			return 0, fmt.Errorf("error while intializing table: %v", err.Error())
+			return nil, fmt.Errorf("error while intializing table: %v", err.Error())
 		}
 	}
+	return db, nil
+}
 
+func Put(rec *Record) (int64, error) {
+	db, err := openOrCreate()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 	cmd := recordToSqlInsert(rec)
+	fmt.Printf("insert %v\n", cmd)
 	result, err := db.Exec(cmd)
 	if err != nil {
 		return 0, fmt.Errorf("error while trying to insert %v: %v",
@@ -78,7 +85,73 @@ func Put(rec *Record) (int64, error) {
 	return id, nil
 }
 
-func Reduce(selector Record, reduceFn func(matches Cursor) interface{}) error {
+func SuccessRate(sel *Record) (float64, error) {
+	db, err := openOrCreate()
+	if err != nil {
+		return 0, err
+	}
+	defer db.Close()
+
+	total := float64(0)
+	success := float64(0)
+	query := selectLikeRecord(sel)
+	fmt.Printf("query %v\n", query)
+	rows, err := db.Query(query)
+	if err != nil {
+		return 0, fmt.Errorf("error while querying %v: %v", query, err.Error())
+	}
+	for rows.Next() {
+		// var service, method, ocID, status, pvType, pvCurr, perfHex, timestamp, pvAmt interface{}
+		var service, method, ocID, status, pvType, pvCurr, timestamp, perfHex []byte
+		var pvAmt int64
+		err := rows.Scan(
+			&service, &method, &timestamp, &ocID, &status, &pvType, &pvAmt, &pvCurr,
+			&perfHex)
+		var rec Record
+
+		if len(service) != 0 {
+			rec.Service = string(service)
+		}
+		if len(method) != 0 {
+			rec.Method = string(method)
+		}
+		if len(ocID) != 0 {
+			rec.OcID = msg.OcID(ocID)
+		}
+		if len(status) != 0 {
+			rec.Status = Status(status)
+		}
+		if len(pvType) != 0 {
+			rec.PaymentType = msg.PaymentType(pvType)
+		}
+		if len(pvCurr) != 0 {
+			pv := msg.PaymentValue{
+				Amount: pvAmt,
+				Currency: msg.Currency(pvCurr),
+			}
+			fmt.Printf("pv %v %v\n", pvAmt, pvCurr)
+			rec.PaymentValue = &pv
+		}
+		if len(perfHex) != 0 {
+			panic("TODO: implement decoding")
+		}
+		if err != nil {
+			return 0, fmt.Errorf("error scanning with %v: %v", query, err.Error())
+		}
+		if rec.Status == SUCCESS || rec.Status == FAILURE {
+			total++;
+		}
+		if rec.Status == SUCCESS {
+			success++;
+		}
+		fmt.Printf("rec: %v\n", rec)
+	}
+
+	return success/total, nil
+}
+
+func Reduce(sel *Record, reduceFn func(rec *Record, res interface{}) interface{}) error {
+	// rows,  = rowsLikeRecord(sel)
 	return nil
 }
 
@@ -108,6 +181,10 @@ func recordFromSqlRow() *Record {
 	return nil
 }
 
+func qesc(s string) string {
+	return strings.Replace(s, "\"", "\\\"", -1)
+}
+
 func recordToSqlInsert(rec *Record) string {
 	var perfHex string
 	if rec.Perf != nil {
@@ -119,14 +196,45 @@ func recordToSqlInsert(rec *Record) string {
 		}
 		perfHex = hex.EncodeToString(buf.Bytes())
 	}
+	pvAmt := int64(0)
+	pvCurr := ""
+	if rec.PaymentValue != nil {
+		pvAmt = rec.PaymentValue.Amount
+		pvCurr = rec.PaymentValue.Currency.String()
+	}
 	return fmt.Sprintf(`
 INSERT INTO rep(service, method, timestamp, ocID, status, paymentType, paymentValueAmount, paymentValueCurrency, perf)
 VALUES ("%s", "%s", "%d", "%s", "%s", "%s", "%d", "%s", x'%s');`, 
 		qesc(rec.Service), qesc(rec.Method), rec.Timestamp, rec.OcID.String(),
-		rec.Status.String(), rec.PaymentType.String(), rec.PaymentValue.Amount,
-		rec.PaymentValue.Currency.String(), perfHex)
+		rec.Status.String(), rec.PaymentType.String(), pvAmt, pvCurr, perfHex)
 }
 
-func qesc(s string) string {
-	return strings.Replace(s, "\"", "\\\"", -1)
+func selectLikeRecord(rec *Record) string {
+	var buf bytes.Buffer
+	buf.WriteString("SELECT service, method, timestamp, ocID, status, paymentType, paymentValueAmount, paymentValueCurrency, perf FROM rep WHERE 1")
+	// buf.WriteString("SELECT status FROM rep WHERE 1")
+	if rec.Service != "" {
+		buf.WriteString(fmt.Sprintf(` AND service = "%s"`, qesc(rec.Service)))
+	}
+	if rec.Method != "" {
+		buf.WriteString(fmt.Sprintf(` AND method = "%s"`, qesc(rec.Method)))
+	}
+	if rec.Timestamp != 0 {
+		buf.WriteString(fmt.Sprintf(` AND timestamp = %d`, rec.Timestamp))
+	}
+	if rec.OcID.String() != "" {
+		buf.WriteString(fmt.Sprintf(` AND ocID = "%s"`, rec.OcID.String()))
+	}
+	if rec.Status.String() != "" {
+		buf.WriteString(fmt.Sprintf(` AND status = "%s"`, rec.Status.String()))
+	}
+	if rec.PaymentType.String() != "" {
+		buf.WriteString(fmt.Sprintf(` AND paymentType = "%s"`,
+			rec.PaymentType.String()))
+	}
+	if rec.PaymentValue != nil {
+		buf.WriteString(fmt.Sprintf(` AND paymentValueAmount = %d AND paymentValueCurreny = "%s"`,
+			rec.PaymentValue.Amount, rec.PaymentValue.Currency.String()))
+	}
+	return buf.String()
 }
--- rep/rep_test.go
+++ rep/rep_test.go
@@ -1,7 +1,6 @@
 package rep
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -10,15 +9,17 @@ import (
 	"github.com/ortutay/decloud/util"
 )
 
-func TestRepPut(t *testing.T) {
+func initDir(t *testing.T) string {
 	dir, err := ioutil.TempDir("", "")
-	defer os.RemoveAll(dir)
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Printf("dir %v\n", dir)
 	util.SetAppDir(dir)
+	return dir
+}
 
+func TestRepPut(t *testing.T) {
+	defer os.RemoveAll(initDir(t))
 	rec := Record{
 		Service:      "store",
 		Method:       "put",
@@ -26,12 +27,42 @@ func TestRepPut(t *testing.T) {
 		OcID:         msg.OcID("id-123"),
 		Status:       SUCCESS,
 		PaymentType:  msg.TXID,
-		PaymentValue: msg.PaymentValue{Amount: 1000, Currency: msg.BTC},
+		PaymentValue: &msg.PaymentValue{Amount: 1000, Currency: msg.BTC},
 		Perf:         nil,
 	}
 
-	_, err = Put(&rec)
+	_, err := Put(&rec)
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestSuccessRate(t *testing.T) {
+	defer os.RemoveAll(initDir(t))
+	id := msg.OcID("id-123")
+	otherID := msg.OcID("id-other")
+	_, err := Put(&Record{OcID: id, Status: SUCCESS, Timestamp: 123})
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = Put(&Record{OcID: id, Status: PENDING})
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = Put(&Record{OcID: id, Status: FAILURE})
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = Put(&Record{OcID: otherID, Status: FAILURE})
+	if err != nil {
+		t.Fatal(err)
+	}
+	sel := Record{OcID: id}
+	rate, err := SuccessRate(&sel)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if .5 != rate {
+		t.Fatal("Expected %v, got %v", .5, rate)
 	}
 }
