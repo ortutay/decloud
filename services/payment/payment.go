494b94d0f03b21768f8e57a11ba8efe49710d91c
ortutay <marcell.ortutay@gmail.com>
2014-04-19 22:46:37 -0700

    rm unused txid method

--- services/payment/payment.go
+++ services/payment/payment.go
@@ -1,10 +1,8 @@
 package payment
 
 import (
-	"fmt"
 	"strings"
 
-	"github.com/conformal/btcjson"
 	"github.com/ortutay/decloud/msg"
 	"github.com/ortutay/decloud/util"
 	"github.com/ortutay/decloud/peer"
@@ -12,7 +10,6 @@ import (
 
 const (
 	SERVICE_NAME        = "payment"
-	TXID_METHOD         = "txid"
 	PAYMENT_ADDR_METHOD = "addr"
 )
 
@@ -35,23 +32,6 @@ func NewPaymentAddrReq(currency msg.Currency) *msg.OcReq {
 	return &msg
 }
 
-// TODO(ortutay): sign these reqs with an input to the txn to prove ownership
-func NewBtcTxidReq(txid msg.BtcTxid) *msg.OcReq {
-	msg := msg.OcReq{
-		ID:          "",
-		Sig:         "",
-		Coins:       []string{},
-		CoinSigs:    []string{},
-		Nonce:       "",
-		Service:     SERVICE_NAME,
-		Method:      TXID_METHOD,
-		Args:        []string{txid.String(), string(msg.BTC)},
-		PaymentType: msg.TXID,
-		Body:        []byte(""),
-	}
-	return &msg
-}
-
 type PaymentService struct {
 	BitcoindConf *util.BitcoindConf
 }
@@ -59,8 +39,6 @@ type PaymentService struct {
 func (ps *PaymentService) Handle(req *msg.OcReq) (*msg.OcResp, error) {
 	methods := make(map[string]func(*msg.OcReq) (*msg.OcResp, error))
 	methods[PAYMENT_ADDR_METHOD] = ps.getPaymentAddr
-	methods[TXID_METHOD] = ps.txid
-
 
 	if method, ok := methods[req.Method]; ok {
 		return method(req)
@@ -93,37 +71,6 @@ func (ps *PaymentService) getPaymentAddr(req *msg.OcReq) (*msg.OcResp, error) {
 		}
 		payAddr := msg.PaymentAddr{Currency: msg.BTC, Addr: btcAddr}
 		return msg.NewRespOk([]byte(payAddr.String())), nil
-	default:
-		return msg.NewRespError(msg.CURRENCY_UNSUPPORTED), nil
-	}
-}
-
-func (ps *PaymentService) txid(req *msg.OcReq) (*msg.OcResp, error) {
-	reqTxid := req.Args[0]
-	reqCurrency := msg.Currency(req.Args[1])
-	switch reqCurrency {
-	case msg.BTC:
-		if ps.BitcoindConf == nil {
-			return msg.NewRespError(msg.SERVER_ERROR), nil
-		}
-		// For now, just see if we can find the transaction
-		cmd, err := btcjson.NewGetTransactionCmd("", string(reqTxid))
-		if err != nil {
-			return nil, fmt.Errorf("error while making cmd: %v", err.Error())
-		}
-		resp, err := util.SendBtcRpc(cmd, ps.BitcoindConf)
-		if err != nil {
-			return msg.NewRespError(msg.SERVER_ERROR), nil
-		}
-		if resp.Error != nil {
-			if resp.Error.Code == -5 {
-				return msg.NewRespError(msg.INVALID_TXID), nil
-			} else {
-				// Catch-all
-				return msg.NewRespError(msg.PAYMENT_DECLINED), nil
-			}
-		}
-		return msg.NewRespOk([]byte("")), nil
 	default:
 		return msg.NewRespError(msg.CURRENCY_UNSUPPORTED), nil
 	}
--- services/payment/payment_test.go
+++ services/payment/payment_test.go
@@ -10,54 +10,6 @@ import (
 	"github.com/ortutay/decloud/testutil"
 )
 
-// func TestTxid(t *testing.T) {
-// 	btcConf, err := util.LoadBitcoindConf("")
-// 	if err != nil {
-// 		t.Errorf("%v", err)
-// 	}
-// 	ps := PaymentService{BitcoindConf: btcConf}
-// 	addrReq := NewPaymentAddrReq(msg.BTC)
-// 	addr, err := ps.getPaymentAddr(addrReq)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	cmd, err := btcjson.NewSendToAddressCmd("", addr, 1e6)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	sendBtcResp, err := util.SendBtcRpc(cmd, btcConf)
-// 	txid, ok := sendBtcResp.Result.(string)
-// 	if !ok {
-// 		log.Fatal(sendBtcResp)
-// 	}
-
-// 	req := NewBtcTxidReq(msg.BtcTxid(txid))
-// 	resp, err := ps.txid(req)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	if resp.Status != msg.OK {
-// 		log.Fatalf("expected %v, but got %v\n", msg.OK, resp.Status)
-// 	}
-// }
-
-// func TestFakeTxidError(t *testing.T) {
-// 	btcConf, err := util.LoadBitcoindConf("")
-// 	if err != nil {
-// 		t.Errorf("%v", err)
-// 	}
-// 	ps := PaymentService{BitcoindConf: btcConf}
-// 	req := NewBtcTxidReq(msg.BtcTxid("fake txid"))
-// 	resp, err := ps.txid(req)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	if resp.Status != msg.INVALID_TXID {
-// 		log.Fatalf("expected %v, but got %v\n", msg.INVALID_TXID, resp.Status)
-// 	}
-// }
-
 func TestGetAddr(t *testing.T) {
 	defer os.RemoveAll(testutil.InitDir(t))
 	btcConf, err := util.LoadBitcoindConf("")
